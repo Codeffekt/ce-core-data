3311ca81e9ec58f43092aef0a9ac408d74452a0e
fabien <fabien.pelisson@codeffekt.com>
2025-02-11 16:08:49 +0100

    add form instance to event

--- src/forms/FormEvent.ts
+++ src/forms/FormEvent.ts
@@ -1,10 +1,11 @@
 import { IndexType } from "../core/core.model";
+import { FormInstance } from "./forms.model";
 
 export type FormEventType = "update";
 
 export interface FormEvent {
     type:  FormEventType;
-    elts: IndexType[];
+    elts: IndexType[] | FormInstance[];
     author: IndexType;
     time: number;
 }
